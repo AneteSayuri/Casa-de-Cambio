9815bb90176eeea9e3bebc5e18abdc6cb01b8e8a
Maria Laura <laura.alves@gocase.com.br>
2023-02-04 15:10:16 -0300

    Feature create microservices project

---
+++ cotacao/src/test/java/com/example/cotacao/CotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
