2e2239736dabb971cfababfb8f2e1222dc588636
AneteS <anete.koga@gmail.com>
2023-02-05 18:28:18 -0300

    Ajuste da Application.properties em todos os modulos.

---
+++ Cotacao/src/main/java/com/ada/cotacao/CotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.ada.cotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CotacaoApplication.class, args);
+    }
+
+}
