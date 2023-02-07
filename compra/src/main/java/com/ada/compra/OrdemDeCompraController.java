9b315299bf58749326b22be5e3ddae5e941f18e1
Maria Laura <laura.alves@gocase.com.br>
2023-02-07 17:49:03 -0300

    Fix controller order purchase

--- compra/src/main/java/com/ada/compra/OrdemDeCompraController.java
+++ compra/src/main/java/com/ada/compra/OrdemDeCompraController.java
@@ -22,8 +22,28 @@ public class OrdemDeCompraController {
     private final OrdemDeCompraService ordemDeCompraService;
 
     @PostMapping
-    public ResponseEntity<OrdemDeCompra> add(@RequestBody OrdemDeCompraDTO ordemDeCompraDTO)  {
-        return ResponseEntity.created(URI.create("/api/ordem-de-compra/" + ordemDeCompraDTO.getId_compra())).build();
+    public ResponseEntity<OrdemDeCompra> add(@RequestBody OrdemDeCompraDTO ordemDeCompra) {
+        if (ordemDeCompra == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma ordem de compra válida");
+        }
+        if (ordemDeCompra.getId_cliente() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma ordem de compra com um cpf atrelado válido.");
+        }
+        if (ordemDeCompra.getTipo_moeda()== null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma  ordem de compra com uma  moeda válida");
+        }
+        if (ordemDeCompra.getNumero_agencia_retirada() == null || ordemDeCompra.getNumero_agencia_retirada().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma ordem de compra com uma agência válida");
+        }
+
+        try {
+            ordemDeCompraService.criarOrdemDeCompra(ordemDeCompra.toEntity());
+        } catch (EntidadeDuplicadaException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Já existe uma ordem de compra com o ID informado");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Ocorreu um erro desconhecido");
+        }
+        return ResponseEntity.created(URI.create("/api/ordem-de-compra/" + ordemDeCompra.getId_compra())).build();
     }
 
     @GetMapping
